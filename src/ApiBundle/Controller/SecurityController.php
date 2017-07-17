80b0914df8ee9f2f10a34615404dd70ec6e59e23
phptuts <noah.php@gmail.com>
2017-07-17 02:10:27 -0500

    adding permissions

--- src/ApiBundle/Controller/SecurityController.php
+++ src/ApiBundle/Controller/SecurityController.php
@@ -48,7 +48,7 @@ class SecurityController extends FOSRestController
     public function getFakeFacebookTokenAction()
     {
         $url = 'https://graph.facebook.com/oauth/access_token?client_id=' . $this->getParameter('facebook_app_id')
-            . '&client_secret=' . $this->getParameter('facebook_app_secret') . '&grant_type=client_credentials&redirect_uri=http://bigfootlocator.com';
+            . '&client_secret=' . $this->getParameter('facebook_app_secret') . '&grant_type=client_credentials&redirect_uri=http://skfsp.info&permissions=email';
 
         $data = json_decode(file_get_contents($url), true);
 
