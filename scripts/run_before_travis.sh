7413874d9530d4275da09d2b2e030480c6f06387
phptuts <noah.php@gmail.com>
2017-07-19 00:17:59 -0500

    trying to do ci with travis

---
+++ scripts/run_before_travis.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+php bin/console doctrine:database:drop --env=test --force
+php bin/console doctrine:database:create --env=test
+php bin/console doctrine:schema:update --env=test --force
+php bin/console doctrine:fixtures:load --env=test --no-interaction
